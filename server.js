aeadea585868c2a8725bd28c81769788201839d2
aschmalzhaf <alexander.schmalzhaf@schmalzhaf.net>
2014-01-29 17:30:16 +0100

    Added socket.io and uploader
    Files are now updated, other clients are notified about update.

--- server.js
+++ server.js
@@ -1,29 +1,105 @@
-var express = require('express');
-var elements = require(__dirname+'/dummy_elements.js');
-var app = express();
 
+var elements = require(__dirname+'/dummy_elements.js');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var fs = require('fs');
+var util = require("util");
 //app.use(express.logger());
+
+app.use(express.bodyParser({uploadDir:'./static/uploads'}));
+
+
+server.listen(80);
+
+
+//socket for pushing notification when new pic is available
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('newPicture', function (data) {
+		console.log("------- New Broadcast");
+		socket.broadcast.emit('newPicture');
+	});
+});
+
 
 
 app.post("/api/upload", function(req, res, next){
-	console.log("File is here");
+
+//SRC: http://www.hacksparrow.com/handle-file-uploads-in-express-node-js.html
+/*
+    // get the temporary location of the file
+    var tmp_path = req.files.myFile.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.thumbnail.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+        });
+    });
+*/
+	
+	
+	var n = req.files.myFile.path.lastIndexOf('\\');
+	console.log('Last Pos: ' + req.files.myFile.path.substring(n + 1));
+
+	
+	io.sockets.emit('newPicture', {"filename":req.files.myFile.path.substring(n + 1)});
+
+
+	res.send('File uploaded: ' + req.files.myFile.path);
+
 })
 
 
+
 app.get("/api/elements", function(req, res, next){
-	console.log(elements.getElements());
 	res.send(elements.getElements());
 })
 
 
 app.use(function(req, res, next){
 	console.log('%s %s', req.method, req.url);
-  	next();
+	next();
 });
+
+
+
 
 
 
 
 app.use(express.static(__dirname + '/static'));
 
-app.listen(80);
+//app.listen(80);
+
+
+/*
+Possible Data Structure
+
+pictures
+	id
+	url
+	createdAt
+	createdBy
+
+users
+	id
+	mailadress
+	name
+	password
+	role
+	createdAt
+	createdBy
+
+
+Ressources:
+http://nodejsdb.org/
+
+
+	*/
--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,146 @@
+
+
+var socket = io.connect('http://localhost');
+socket.on('news', function (data) {
+  console.log(data);
+});
+
+
+
 //inject angular file upload directives and service.
-var app = angular.module('app', ['angularFileUpload']);
+var app = angular.module('app', ['angularFileUpload']); 
+
+//directive for scrolling down list everytime
+app.directive('scrolldown', function() {
+  return function(scope, element, attrs, $anchorScroll) {
+    if (scope.$last){
+      //window.alert("im the last!");
+      //$anchorScroll();
+      console.log(element.parent());
+      location.hash = "#last";
+    }
+  };
+})
 
 
-
-app.controller('newsController', function($scope, $http){
+app.controller('newsController', function($scope, $location, $anchorScroll, $http){
   $http.get('/api/elements').success(function(data){
     console.log(data);
     $scope.elements = data;
   });
+  $scope.notifyPic = function(){
+    socket.emit('newPicture');
+  }
+
+  $scope.gotoLast = function (){
+    $location.hash('last');
+    $anchorScroll();
+  }
+
+  socket.on('newPicture', function (data) {
+    $scope.$apply(function(){
+      $scope.elements.push({"ID":data.filename, "type":"img", "value":data.filename}); 
+      $location.hash('last');
+      $anchorScroll();
+    })
+
+  });
 });
 
 
-var uploadController = [ '$scope', '$upload', function($scope, $upload) {
-  $scope.onFileSelect = function($files) {
-    console.log("---- File selected");
-    //$files: an array of files selected, each file has name, size, and type.
-    for (var i = 0; i < $files.length; i++) {
-      var file = $files[i];
-      $scope.upload = $upload.upload({
-        url: '/api/upload', //upload.php script, node.js route, or servlet url
-        // method: POST or PUT,
-        // headers: {'headerKey': 'headerValue'}, withCredential: true,
-        data: {myObj: $scope.myModelObj},
-        file: file,
-        // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
-        /* set file formData name for 'Content-Desposition' header. Default: 'file' */
-        //fileFormDataName: myFile,
-        /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
-        //formDataAppender: function(formData, key, val){} 
-      }).progress(function(evt) {
-        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-      }).success(function(data, status, headers, config) {
-        // file is uploaded successfully
-        console.log(data);
-      });
-      //.error(...)
-      //.then(success, error, progress); 
-    }
+
+//---------------------------
+//  Upload
+//---------------------------
+
+var uploadController = [ '$scope', '$http','$upload', '$timeout', function($scope, $http, $upload,$timeout) {
+  $scope.fileReaderSupported = window.FileReader != null;
+  $scope.uploadRightAway = true;
+  $scope.changeAngularVersion = function() {
+    window.location.hash = $scope.angularVersion;
+    window.location.reload(true);
+  }
+  $scope.hasUploader = function(index) {
+    return $scope.upload[index] != null;
   };
+  $scope.abort = function(index) {
+    $scope.upload[index].abort(); 
+    $scope.upload[index] = null;
+  };
+
+
+  $scope.onFileSelect = function($files) {
+    $scope.selectedFiles = [];
+    $scope.progress = [];
+    if ($scope.upload && $scope.upload.length > 0) {
+      for (var i = 0; i < $scope.upload.length; i++) {
+        if ($scope.upload[i] != null) {
+          $scope.upload[i].abort();
+        }
+      }
+    }
+    $scope.upload = [];
+    $scope.uploadResult = [];
+    $scope.selectedFiles = $files;
+    $scope.dataUrls = [];
+    for ( var i = 0; i < $files.length; i++) {
+      var $file = $files[i];
+      if (window.FileReader && $file.type.indexOf('image') > -1) {
+        var fileReader = new FileReader();
+        fileReader.readAsDataURL($files[i]);
+        function setPreview(fileReader, index) {
+          fileReader.onload = function(e) {
+
+            $timeout(function() {
+              $scope.dataUrls[index] = e.target.result;
+            });
+            
+          }
+        }
+        setPreview(fileReader, i);
+      }
+      $scope.progress[i] = -1;
+      if ($scope.uploadRightAway) {
+        $scope.start(i);
+      }
+    }
+  }
+
+  $scope.howToSend = 1;
+
+
+  $scope.start = function(index) {
+    $scope.progress[index] = 0;
+    if ($scope.howToSend == 1) {
+      $scope.upload[index] = $upload.upload({
+        url : '/api/upload',
+        method: $scope.httpMethod,
+        file: $scope.selectedFiles[index],
+        fileFormDataName: 'myFile'
+      }).then(function(response) {
+        $scope.uploadResult.push(response.data.result);
+      }, null, function(evt) {
+        $scope.progress[index] = parseInt(100.0 * evt.loaded / evt.total);
+      });
+    } else {
+      var fileReader = new FileReader();
+      fileReader.readAsArrayBuffer($scope.selectedFiles[index]);
+      fileReader.onload = function(e) {
+        $scope.upload[index] = $upload.http({
+          url: '/api/upload',
+          headers: {'Content-Type': $scope.selectedFiles[index].type},
+          data: e.target.result
+        }).then(function(response) {
+          $scope.uploadResult.push(response.data.result);
+        }, null, function(evt) {
+          // Math.min is to fix IE which reports 200% sometimes
+          $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+      }
+    }
+  }
+
 }];
+
+
+
